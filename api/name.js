62e49ff31adf5331b16d9daa591249961d217065
emirontop1 <dincemr@gmail.com>
2025-09-14 18:46:45 +0300

    Update name.js

--- api/name.js
+++ api/name.js
@@ -1,6 +1,16 @@
 export default function handler(req, res) {
+  // CORS izinleri
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Her domain izinli
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    // Preflight request için 200 dön
+    return res.status(200).end();
+  }
+
   if (req.method === "POST") {
-    const { name } = req.body; // Vercel default parser
+    const { name } = req.body;
     if (!name) return res.status(400).json({ error: "İsim yok" });
 
     if (name === "Emir") {
